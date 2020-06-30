160ac43043be1026ae1e1c2dcc3e52a0ff8c0e18
gordil11 <alvaro.gordilloreina@aareon.com>
2020-06-30 10:57:34 +0200

    dos

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,8 @@ import * as admin from 'firebase-admin';
 
 const serviceAccount = require('../pushnotificationnl-firebase-adminsdk-q5n1c-83a4919537.json');
 
+console.log('DATA', serviceAccount);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://pushnotificationnl.firebaseio.com',
